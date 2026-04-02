63fb737afdf67bfcf5478b1af9a3568241563735
Harsh Diwakar <71271113+zzharsh@users.noreply.github.com>
2026-04-02 09:26:18 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isValid(string x)
+    {
+        cout<<"Called isValid with "<<x<<" ";
+        int y = stoi(x);
+        printf("Converted %s --> %d: ", x.c_str(), y);
+        if(x[0]=='0')
+        {
+            cout<<0<<endl;
+            return false;
+        }
+        else if(y > 26 || y < 1)
+        {
+            cout<<0<<endl;
+            return false;
+        }
+        cout<<1<<endl;
+        return true;
+    }
+    int numDecodings(string s) {
+        int res = 0;
+        int n = s.size();
+        vector<int> DP(n+1, 0);
+        DP[n-1] = s[n-1] == '0' ? 0 : 1;
+        DP[n] = 1;
+        for(int i = n-2; i >= 0; --i)
+        {
+            string sub1 = "";
+            sub1 += s[i]; 
+            // sub1 += s[i];
+            string sub2 = sub1 + s[i+1];
+            cout<<i<<" "<<sub1<<" "<<sub2<<" "<<s[i]<<endl;
+            if(isValid(sub1))
+            {
+                DP[i] += DP[i+1];
+            }
+            if(isValid(sub2))
+            {
+                DP[i] += DP[i+2];
+            }
+
+        }
+        return DP[0];
+    }
+};
