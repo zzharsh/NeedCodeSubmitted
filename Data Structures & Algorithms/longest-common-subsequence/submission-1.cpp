b236a542e9cc8cf2f974d84fc812ff8bb0519b7e
Harsh Diwakar <71271113+zzharsh@users.noreply.github.com>
2026-04-03 08:28:22 +0530

    Add: longest-common-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-1.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int res = 0;
+        int m = text1.size();
+        int n = text2.size();
+        vector<vector<int>> DP(m+1, vector<int>(n+1,0));
+        for(int i = m-1; i > -1; --i)
+        {
+            for(int j = n-1; j > -1; --j)
+            {
+                if(text1[i] == text2[j])
+                {
+                    DP[i][j] = DP[i+1][j] + 1;
+                }
+                else
+                {
+                    DP[i][j] = max(DP[i+1][j], DP[i][j+1]);
+                }
+            }
+        }
+        printf("| 0 |");
+        for(int i = 0; i < m; ++i)
+        {
+            printf("| %c |", text1[i]);
+            // for(int j = 0; j < n; ++j)
+            // {
+            //     printf(text1[i], text2[j])
+            // }
+        }
+        cout<<endl;
+        vector<bool> flags(n);
+        for(int i = 0; i < m; ++i)
+        {
+            for(int j = 0; j < n; ++j)
+            {
+                // if(!flags[j])
+                // {
+                //     printf("| %c |", text2[j]);
+                //     flags[j] = true;
+                // }
+                printf("| %d |", DP[i][j]);
+            }
+            cout<<endl;
+        }
+        return DP[0][0];
+    }
+};
