7d3941e4fa17b414d51fa7cef1a794aaadd39616
Harsh Diwakar <71271113+zzharsh@users.noreply.github.com>
2026-04-06 09:48:57 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> DP(n+1, 0);
+        if(n == 1)
+        {
+            return nums[0];
+        }
+        if(n == 2)
+        {
+            return max(nums[0], nums[1]);
+        }
+        DP[n] = 0;
+        DP[n-1] = -1 * nums[n-1];
+        DP[n-2] = nums[n-2];
+        int res = max(abs(DP[n-1]), abs(DP[n-2]));
+        for(int i = n - 3; i > 0; --i)
+        {
+            int a = abs(DP[i+2]);
+            int b = abs(DP[i+3]);
+            DP[i] = max(a,b) + nums[i];
+            res = max(res, DP[i]);
+            if(a > b && DP[i+2] < 0)
+            {
+                DP[i] *= -1;
+            }
+            if(a < b && DP[i+3] < 0)
+            {
+                DP[i] *= -1;
+            }
+            printf(" DP[%d] = %d\n", i, DP[i]);
+        }
+        int a = 0, b = 0, i = 0;
+        if(DP[i + 2] < 0)
+        {
+            a = nums[i] + abs(DP[i+2]) - nums[n-1];
+        }
+        else
+        {
+            a = nums[i] + abs(DP[i+2]);
+        }
+        if(DP[i + 3] < 0)
+        {
+            b = nums[i] + abs(DP[i+3]) - nums[n-1];
+        }
+        else
+        {
+            b = nums[i] + DP[i+3];
+        }
+        DP[i] = max(a,b);
+        res = max(DP[i], res);
+        return res;
+        // int a = abs(DP[i+2]);
+        // int b = abs(DP[i+3]);
+        // DP[i] = max(a,b) + nums[i];
+        // res = max(res, DP[i]);
+        // if(a > b && DP[i+2] < 0)
+        // {
+        //     DP[i][j] *= -1;
+        // }
+        // if(a < b && DP[i+3] < 0)
+        // {
+        //     DP[i][j] *= -1;
+        // }
+    }
+};
