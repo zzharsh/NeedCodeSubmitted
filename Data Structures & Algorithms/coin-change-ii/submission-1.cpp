ce9b4717dfce5a47a20f7437c89b6f6ae058cb66
Harsh Diwakar <71271113+zzharsh@users.noreply.github.com>
2026-04-17 09:45:24 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        if(amount == 0)
+        {
+            return 0;
+        }
+        int n = coins.size();
+        sort(coins.begin(), coins.end());
+        vector<vector<int>> DP(amount + 1, vector<int>(n, 0));
+        for(int i = 0; i < amount+1; ++i)
+        {
+            for(int j = n-1; j > -1; --j)
+            {
+                if(i < coins[j])
+                {
+                    DP[i][j] = 0;
+                }   
+                else if( i == coins[j])
+                {
+                    DP[i][j] = 1;
+                }
+                else
+                {
+                    if(j+1 < n)
+                    {
+                        DP[i][j] = DP[i - coins[j]][j] + DP[i][j+1];
+                    }
+                    else
+                    {
+                        DP[i][j] = DP[i - coins[j]][j];
+                        //  + DP[i][j+1];
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < amount+1; ++i)
+        {
+            for(int j = 0; j < n; ++j)
+            {
+                printf("%d ", DP[i][j]);
+
+            }
+            cout<<endl;
+        }
+        return DP[amount][0];
+    }
+};
