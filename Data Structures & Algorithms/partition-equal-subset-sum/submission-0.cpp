f9219f9b1945cfe0f7c021b470321a3bf24667b2
Harsh Diwakar <71271113+zzharsh@users.noreply.github.com>
2026-04-19 18:42:57 +0530

    Add: partition-equal-subset-sum - submission-0

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-0.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0, n = nums.size(), i;
+        for (i = 0; i < n; ++i)
+        {
+            sum += nums[i];
+        }
+        if(sum % 2 != 0)
+        {
+            return false;
+        }
+        vector<bool> DP1(sum / 2 + 1, false), DP2(sum / 2 + 1, false);
+        for(i = n-1; i >= 0; --i)
+        {
+            printf("%d | ", i, nums[i]);
+            for(int j = 0; j < sum/2 + 1; ++j)
+            {
+                if( j == nums[i])
+                {
+                    DP1[j] = true;
+                }
+                if (true == DP2[j])
+                {
+                    DP1[j] = true;
+                    if(nums[i] + j < sum / 2 + 1)
+                    {
+                        DP1[nums[i] + j] = true;
+                    }
+                }
+                printf("%d:%d %d, ", j, static_cast<int>(DP1[j]), nums[i] + j);
+                if(DP1[sum/2] == true)
+                {
+                    return true;
+                }
+            }
+            cout<<endl;
+            DP2 = DP1;
+            // DP1 = std::move(vector<bool>(sum / 2 + 1, false));
+        }
+        return false;
+    }
+};
